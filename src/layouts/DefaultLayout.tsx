3e1ace3b5cf348801ab3dc884039c820e9afb34a
jithu.majinu <jithu.majinu@gmail.com>
2022-09-20 00:14:05 +0900

    rsuit fixed

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -2,9 +2,24 @@ import React, { FC } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import HomeIcon from '@rsuite/icons/legacy/Home';
 import { Container, Sidebar, Sidenav, Content, Nav, DOMHelper, Row } from 'rsuite';
+import { Icon } from '@rsuite/icons';
+
+import NavLink from '../component/NavLink';
+
 export interface DefaultLayoutProps {
   children?: React.ReactNode;
 }
+
+//const App <AppProps>= () => {
+
+const NavItem = (props: any) => {
+  const { title, eventKey, ...rest } = props;
+  return (
+    <Nav.Item eventKey={eventKey} as={NavLink} {...rest}>
+      {title}
+    </Nav.Item>
+  );
+};
 
 export const DefaultLayout: FC<DefaultLayoutProps> = props => {
   return (
@@ -12,9 +27,17 @@ export const DefaultLayout: FC<DefaultLayoutProps> = props => {
       <Row>
         <div className="menu-bar">
           <Nav>
-            <Nav.Item href='/' icon={<HomeIcon/>}>Home</Nav.Item>
-            <Nav.Item href='/dashboard'  >News</Nav.Item>
-            <Nav.Item href='/home' >Solutions</Nav.Item>
+            {/* <Nav.Item key="/" icon={<HomeIcon />}>
+              Home
+            </Nav.Item> */}
+            <NavItem
+              key="dashboard"
+              title="Dashboard"
+              to="/dashboard"
+              icon={<HomeIcon />}
+            />
+            {/* <Nav.Item key="dashboard">Dashboard</Nav.Item> */}
+            <Nav.Item key="home">home</Nav.Item>
             <Nav.Item>Products</Nav.Item>
             <Nav.Menu title="Item E">
               <Nav.Item>Item E-1</Nav.Item>
