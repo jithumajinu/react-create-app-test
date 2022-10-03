e0b55d08ef019369d92837c6da2d7abb9eb09456
jithu.majinu <jithu.majinu@gmail.com>
2022-10-03 11:51:47 +0900

    grid

--- src/component/ProgressBar.tsx
+++ src/component/ProgressBar.tsx
@@ -3,17 +3,22 @@ export const ProgressBar = () => {
     // <div className="container">
     //   <div className="bar"></div>
     // </div>
-    <main>
-      <h1>
-        Loading<span className="dot">.</span>
-        <span className="dot">.</span>
-        <span className="dot">.</span>
-        <sub>infinitely :)</sub>
-      </h1>
-      <div id="container">
-        <div id="bar"></div>
-      </div>
-    </main>
+    // <main>
+    //   <h1>
+    //     Loading<span className="dot">.</span>
+    //     <span className="dot">.</span>
+    //     <span className="dot">.</span>
+    //     <sub>infinitely :)</sub>
+    //   </h1>
+    //   <div id="container">
+    //     <div id="bar"></div>
+    //   </div>
+
+    // </main>
+
+    <div className="container">
+      <div className="bar"></div>
+    </div>
   );
 };
 
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,13 +1,52 @@
 import React from 'react';
-import { Panel } from 'rsuite';
+import { Panel, Grid, Row, Col } from 'rsuite';
 import Dashboard from './Dashboard';
 import { ProgressBar } from '../../component/ProgressBar';
+
+import './grid.css';
 
 const Page = () => {
   return (
     <Panel header={<h3 className="title">Dashboard ddd</h3>}>
       <ProgressBar />
       <Dashboard />
+
+      <Grid fluid>
+        <Row className="show-grid">
+          <Col xs={2}>xs={2}</Col>
+          <Col xs={2}>xs={2}</Col>
+          <Col xs={2}>xs={2}</Col>
+          <Col xs={2}>xs={2}</Col>
+          <Col xs={2}>xs={2}</Col>
+          <Col xs={2}>xs={2}</Col>
+          <Col xs={2}>xs={2}</Col>
+          <Col xs={2}>xs={2}</Col>
+          <Col xs={2}>xs={2}</Col>
+          <Col xs={2}>xs={2}</Col>
+          <Col xs={2}>xs={2}</Col>
+          <Col xs={2}>xs={2}</Col>
+        </Row>
+
+        <Row className="show-grid">
+          <Col xs={4}>xs={4}</Col>
+          <Col xs={4}>xs={4}</Col>
+          <Col xs={4}>xs={4}</Col>
+          <Col xs={4}>xs={4}</Col>
+          <Col xs={4}>xs={4}</Col>
+          <Col xs={4}>xs={4}</Col>
+        </Row>
+
+        <Row className="show-grid">
+          <Col xs={8}>xs={8}</Col>
+          <Col xs={8}>xs={8}</Col>
+          <Col xs={8}>xs={8}</Col>
+        </Row>
+
+        <Row className="show-grid">
+          <Col xs={12}>xs={12}</Col>
+          <Col xs={12}>xs={12}</Col>
+        </Row>
+      </Grid>
     </Panel>
   );
 };
