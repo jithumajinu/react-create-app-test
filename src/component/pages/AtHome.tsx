8a3acf180fa2be129f90860e8e78492e6ee8d79b
jithu.majinu <jithu.majinu@gmail.com>
2022-10-19 10:29:48 +0900

    Pagination

--- src/component/pages/AtHome.tsx
+++ src/component/pages/AtHome.tsx
@@ -1,40 +1,39 @@
-import { Link } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext";
-
+import { Link } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+import { Pagination } from 'rsuite';
 
 export const AtHome = () => {
-
   const { currentUser } = useAuth();
 
-  console.log('%c: currentUser', 'color:yellow', currentUser );
+  console.log('%c: currentUser', 'color:yellow', currentUser);
+  const [activePage, setActivePage] = React.useState(5);
 
   return (
     <div className="at-home">
-       
-      home page 
-      See to experience
-information Technology
-Content is the king in today's world. It drives interactions in a customer-oriented
-market with relevance and consistency. That said, content without context is devoid
-of purpose and means nothing. This is where C2Xi enters the picture.
-
-See to experience
-information Technology
-Content is the king in today's world. It drives interactions in a customer-oriented
-market with relevance and consistency. That said, content without context is devoid
-of purpose and means nothing. This is where C2Xi enters the picture.
-
-See to experience
-information Technology
-Content is the king in today's world. It drives interactions in a customer-oriented
-market with relevance and consistency. That said, content without context is devoid
-of purpose and means nothing. This is where C2Xi enters the picture.
-
-See to experience
-information Technology
-Content is the king in today's world. It drives interactions in a customer-oriented
-market with relevance and consistency. That said, content without context is devoid
-of purpose and means nothing. This is where C2Xi enters the picture.
+      <Pagination
+        prev
+        last
+        next
+        first
+        size="lg"
+        total={100}
+        limit={10}
+        activePage={activePage}
+        onChangePage={setActivePage}
+      />
+      home page See to experience information Technology Content is the king in today's world. It
+      drives interactions in a customer-oriented market with relevance and consistency. That said,
+      content without context is devoid of purpose and means nothing. This is where C2Xi enters the
+      picture. See to experience information Technology Content is the king in today's world. It
+      drives interactions in a customer-oriented market with relevance and consistency. That said,
+      content without context is devoid of purpose and means nothing. This is where C2Xi enters the
+      picture. See to experience information Technology Content is the king in today's world. It
+      drives interactions in a customer-oriented market with relevance and consistency. That said,
+      content without context is devoid of purpose and means nothing. This is where C2Xi enters the
+      picture. See to experience information Technology Content is the king in today's world. It
+      drives interactions in a customer-oriented market with relevance and consistency. That said,
+      content without context is devoid of purpose and means nothing. This is where C2Xi enters the
+      picture.
     </div>
   );
 };
